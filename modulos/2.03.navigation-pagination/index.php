5c89e764c1facce10eca2fc22d52c021226dfcf3
Sandra Segato <sandra@a2comunicacao.com.br>
2013-02-06 13:13:52 -0200

    Atualizando modulos 2.03

--- modulos/2.03.navigation-pagination/index.php
+++ modulos/2.03.navigation-pagination/index.php
@@ -1,7 +1,7 @@
-<div class="pagination">
+<nav class="site-pagination">
 	<a href="#" class="prev">&larr; Anterior</a>
 	<a href="#">1</a>
 	<a href="#" class="active">2</a>
 	<a href="#">3</a>
 	<a href="#" class="next">&rarr; Pr&oacute;ximo</a>
-</div>
+</nav>

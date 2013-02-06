1e274e9ce4efbc3b131b9447f7e5f644b25ff816
Sandra Segato <sandra@a2comunicacao.com.br>
2013-02-06 15:05:54 -0200

    Atualizando modulos 2.04

--- modulos/2.04.navigation-breadcrumbs/index.php
+++ modulos/2.04.navigation-breadcrumbs/index.php
@@ -1,9 +1,7 @@
-<nav id="breadcrumbs">
-	<ul>
-		<li>
-			<li class="inicio"><a href="/">In&iacute;cio</a></li>
-			<li><a href="#">Nome da se&ccedil;&atilde;o</a></li>
-			<li class="current">Nome da p&aacute;gina atual</li>
-		</li>
+<nav class="site-breadcrumbs">
+	<ul id="breadcrumbs">
+		<li class="startpage"><a href="/">In&iacute;cio</a></li>
+		<li><a href="#">Nome da se&ccedil;&atilde;o</a></li>
+		<li class="current">Nome da p&aacute;gina atual</li>
 	</ul>
 </nav>

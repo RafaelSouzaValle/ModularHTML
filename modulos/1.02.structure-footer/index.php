868f1b6cd9f58d08f56fd8c1d38461310d2fb911
Sandra Segato <sandra@a2comunicacao.com.br>
2013-02-06 12:33:30 -0200

    Atualizando modulos 1.02

--- modulos/1.02.structure-footer/index.php
+++ modulos/1.02.structure-footer/index.php
@@ -1,4 +1,4 @@
 <footer>
 	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-	<p class="copyRight">Copyright &copy; 2013 Nome do Cliente</p> <!-- UTF8:  © -->
+	<p class="copyRight">Copyright &copy; 2013 Nome do Cliente</p>
 </footer>

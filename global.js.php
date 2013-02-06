c91714b7d010e61d04f56da37e8f8971ab645d3c
Sandra Segato <sandra@a2comunicacao.com.br>
2013-02-06 15:28:03 -0200

    Atualizando modulos 2.05

--- global.js.php
+++ global.js.php
@@ -4,20 +4,20 @@
 <script type="text/javascript">
 $(document).ready(function(){
 
-	$('#tab-content .tab:first-child').css('display', 'block');
+	$('#tab-content .tab-content:first-child').css('display', 'block');
 	$('#tab-nav li:first-child a').addClass('current');
 	
-	var alturaInicial = $('#tab-content .tab:first-child').innerHeight();
+	var alturaInicial = $('#tab-content .tab-content:first-child').innerHeight();
 	$('#tab-content').height(alturaInicial);
 
 	$('a', $('#tab-nav')).click(function() {
 		var i = $('a', $('#tab-nav') ).index(this) + 1;
-		$(this).parents('#tabs').children('#tab-content').children('.tab:visible').hide();
-		$('#tab' + i).show();
+		$(this).parents('#tabs').children('#tab-content').children('.tab-content:visible').hide();
+		$('#tab-' + i).show();
 		$(this).parents('#tab-nav').find('a').removeClass('current');
 		$(this).addClass('current');
 		
-		var altura = $('#tab' + i ).innerHeight();
+		var altura = $('#tab-' + i ).innerHeight();
 		$('#tab-content').stop().animate({
 			height: altura
 		}, 'slow' );

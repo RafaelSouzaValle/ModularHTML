358fd07c4feabed256fc8e4c086ceafdcfc5bb06
Sandra Segato <sandra@a2comunicacao.com.br>
2013-02-08 15:24:28 -0200

    Atualizando módulos 2.06.navigation-carousel e 4.03.image-slideshow

--- modulos/2.06.navigation-carousel/index.php
+++ modulos/2.06.navigation-carousel/index.php
@@ -1,16 +1,28 @@
 <div id="carousel" class="flexslider site-carousel">
 	<ul class="slides">
-		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
-		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
-		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
-		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
-		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
-		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
-		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
-		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
-		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
-		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
-		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
-		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
+		<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem"></a></li>
 	</ul>
 </div>
--- modulos/4.03.image-slideshow/index.php
+++ modulos/4.03.image-slideshow/index.php
@@ -1,11 +1,11 @@
 <div id="slideshow" class="flexslider site-slideshow">
 	<ul class="slides">
 		<li>
-			<img src="http://placehold.it/650x400&text=Foto1" />
+			<img src="http://placehold.it/650x400&text=Foto1" alt="Imagem Slide">
 			<p class="flex-caption">Legenda da imagem</p>
 		</li>
 		<li>
-			<img src="http://placehold.it/650x400&text=Foto2" />
+			<img src="http://placehold.it/650x400&text=Foto2" alt="Imagem Slide">
 			<p class="flex-caption">Legenda da imagem</p>
 		</li>
 	</ul>

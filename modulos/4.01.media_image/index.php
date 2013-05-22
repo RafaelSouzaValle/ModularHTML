d17a4507b136abd19684090c769785695b1eb663
Sandra Segato <sandra@a2comunicacao.com.br>
2013-05-22 11:41:03 -0300

    Atualizando 4.01.media_image

--- modulos/4.01.media_image/index.php
+++ modulos/4.01.media_image/index.php
@@ -1,4 +1,4 @@
-<figure>
-	<img src="http://placehold.it/450x300" alt="Descri&ccedil;&atilde;o da imagem">
-	 <figcaption>Inserir legenda</figcaption>
+<figure class="media_image">
+	<img src="http://placehold.it/640x300" alt="Descri&ccedil;&atilde;o da imagem">
+	<figcaption>Inserir legenda</figcaption>
 </figure>
